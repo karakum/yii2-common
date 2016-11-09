93ba86ef181604cc4eeab5e3ff15dcca74c72dd8
Andrey Shertsinger <andrey@shertsinger.ru>
2016-11-10 01:05:21 +0700

    i18n cropper modal

--- src/cropper/views/modal.php
+++ src/cropper/views/modal.php
@@ -12,9 +12,9 @@ use yii\web\View;
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                <button type="button" class="close" data-dismiss="modal" aria-label="<?= Yii::t('cropper', 'Close') ?>"><span
                         aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Select crop area and click "Crop" button</h4>
+                <h4 class="modal-title"><?= Yii::t('cropper', 'Select crop area and click "Crop" button') ?></h4>
             </div>
             <div class="modal-body">
                 <div class="crop-image-container">
@@ -22,8 +22,8 @@ use yii\web\View;
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary crop-submit">Crop</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"><?= Yii::t('cropper', 'Close') ?></button>
+                <button type="button" class="btn btn-primary crop-submit"><?= Yii::t('cropper', 'Crop') ?></button>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
